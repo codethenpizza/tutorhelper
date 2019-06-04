282c7c0be5fb6e942fc5effecf2baad3a2f0d50f
HairCrusher <fitality1@gmail.com>
2019-06-04 20:52:35 +0300

    Drawer and Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,56 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('TutorHelper'),
       ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Главная'),
+              selected: true,
+              onTap: () {
+
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('Ученики'),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_time),
+              title: Text('Расписание'),
+            ),
+            ListTile(
+              leading: Icon(Icons.archive),
+              title: Text('Архив'),
+            ),
+            ListTile(
+              leading: Icon(Icons.equalizer),
+              title: Text('Статистика'),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Настройки'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.feedback),
+                    title: Text('Обратная связь'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       body: Dashboard()
 
     );
--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,15 +1,83 @@
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
-
   @override
   State createState() => DashboardState();
 }
 
-class DashboardState extends State<Dashboard>{
+class DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+        ClassCard(),
+      ],
+    );
+  }
+}
+
+class ClassCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    return Card(
+      child: ExpansionTile(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: Center(child: Text('asdas'),),
+          )
+        ],
+        title: Padding(
+          padding: EdgeInsets.all(0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.deepOrange,
+                  child: Center(
+                    child: Text('М', style: TextStyle(fontSize: 30, color: Colors.white),),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Иван Иванов', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                    Text('120 минут - 2000р'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text('12:20'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
