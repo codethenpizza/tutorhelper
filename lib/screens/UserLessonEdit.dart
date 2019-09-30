ae0f076456498b0237008a3abcd961971b4c9d28
Alex Shu <>
2019-09-30 13:30:55 +0300

    -delete padding on user lesson create screen

--- lib/screens/UserLessonEdit.dart
+++ lib/screens/UserLessonEdit.dart
@@ -69,93 +69,219 @@ class UserLessonEditState extends State<UserLessonEdit> {
   }
 
 
+//  @override
+//  Widget build(BuildContext context) => Container(
+//          child: new Stack(children: <Widget>[
+//        new Container(
+//          child: new Image.asset(
+//            'assets/appbarfullbg.jpg',
+//            fit: BoxFit.cover,
+//            height: MediaQuery.of(context).size.height,
+//          ),
+//          color: Colors.lightGreen,
+//        ),
+//        Scaffold(
+//          backgroundColor: Colors.transparent,
+//          appBar: AppBar(
+//            title: Text('TutorHelper'),
+//            backgroundColor: Colors.transparent,
+//            elevation: 0.0,
+//            actions: <Widget>[
+//              IconButton(icon: Icon(Icons.delete), onPressed: removeUserLesson)
+//            ],
+//          ),
+//          body: Builder(builder: (context) {
+//            return Padding(
+//              padding: const EdgeInsets.all(8.0),
+//              child:
+//              Form(
+//                key: _formKey,
+//                child: ListView(
+//                children: <Widget>[
+//                  CardLayout(
+//                    children: <Widget>[
+//                      ColorPicker(
+//                        onMainColorChange: (ColorSwatch color) {
+//                          _color = color.value;
+//                        },
+//                        elevation: 10,
+//                        selectedColor: Color(_color),
+//                        allowShades: false,
+////                selectedColor: Colors.red,
+//                        colors: [
+//                          Colors.red,
+//                          Colors.deepOrange,
+//                          Colors.yellow,
+//                          Colors.lightGreen,
+//                          Colors.blueAccent,
+//                        ],
+//                        circleSize: 40,
+//                      ),
+//                      TextFormField(
+//                        controller: nameController,
+//                        validator: (String value) {
+//                          if (value.isEmpty) {
+//                            return "Название не может быть пустым";
+//                          }
+//                        },
+//                        decoration: InputDecoration(
+//                          labelText: 'Название',
+//                        ),
+//                      ),
+//                      TextField(
+//                        controller: descController,
+//                        decoration: InputDecoration(
+//                          labelText: 'Описание',
+//                        ),
+//                        minLines: 2,
+//                        maxLines: 5,
+//                      ),
+//                    ],
+//                  ),
+//                  CardLayout(
+//                    children: <Widget>[
+//                      TextFormField(
+//                        controller: durationController,
+//                        validator: (String value) {
+//                          if (value.isEmpty && value != null) {
+//                            return "Укажите длительность";
+//                          }
+////                                      else if (value ){
+////                                        return "Укажите длительность";
+////                                      }
+//                        },
+//                        decoration: InputDecoration(
+//                          labelText: 'Длительность в минутах',
+//                        ),
+//                        keyboardType: TextInputType.number,
+//                      ),
+//                      TextFormField(
+//                        controller: costController,
+//                        validator: (String value) {
+//                          if (value.isEmpty && value != null) {
+//                            return "Укажите цену урока";
+//                          }
+////                                      else if (value ){
+////                                        return "Укажите длительность";
+////                                      }
+//                        },
+//                        decoration: InputDecoration(
+//                          labelText: 'Цена',
+//                        ),
+//                        keyboardType: TextInputType.number,
+//                      ),
+//                      Padding(
+//                        padding: const EdgeInsets.only(top: 10.0),
+//                        child: RaisedButton(
+//                          onPressed: () {
+//                            setState(() {
+//                              if (_formKey.currentState.validate()) {
+//                                editLesson(context);
+//                              }
+//                            });
+//                          },
+//                          child: Row(
+//                            mainAxisAlignment: MainAxisAlignment.center,
+//                            children: <Widget>[
+//                              Text(
+//                                'Обновить',
+//                                style: TextStyle(
+//                                    color: Theme.of(context).backgroundColor,
+//                                    fontSize: 17),
+//                              ),
+//                            ],
+//                          ),
+//                        ),
+//                      ),
+//                    ],
+//                  )
+//                ],
+//              ),
+//              ),
+//            );
+//          }),
+//        )
+//      ]));
+
   @override
-  Widget build(BuildContext context) => Container(
-          child: new Stack(children: <Widget>[
-        new Container(
-          child: new Image.asset(
-            'assets/appbarfullbg.jpg',
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height,
-          ),
-          color: Colors.lightGreen,
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: Text('TutorHelper'),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            actions: <Widget>[
-              IconButton(icon: Icon(Icons.delete), onPressed: removeUserLesson)
-            ],
-          ),
-          body: Builder(builder: (context) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:
-              Form(
-                key: _formKey,
-                child: ListView(
-                children: <Widget>[
-                  CardLayout(
-                    children: <Widget>[
-                      ColorPicker(
-                        onMainColorChange: (ColorSwatch color) {
-                          _color = color.value;
-                        },
-                        elevation: 10,
-                        selectedColor: Color(_color),
-                        allowShades: false,
-//                selectedColor: Colors.red,
-                        colors: [
-                          Colors.red,
-                          Colors.deepOrange,
-                          Colors.yellow,
-                          Colors.lightGreen,
-                          Colors.blueAccent,
+  Widget build(BuildContext context) {
+    return Layout(
+        title: "Создание урока",
+        child:
+        Form(
+          key: _formKey,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    color: Theme.of(context).backgroundColor,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: <Widget>[
+                          //space inside block
+                          ColorPicker(
+                            elevation: 10,
+                            shrinkWrap: true,
+                            onMainColorChange: (ColorSwatch color) {
+                              _color = color.value;
+                            },
+                            selectedColor: Color(_color),
+                            allowShades: false,
+                            colors: [
+                              Colors.red,
+                              Colors.deepOrange,
+                              Colors.yellow,
+                              Colors.lightGreen,
+                              Colors.blueAccent,
+                            ],
+                            circleSize: 40,
+                          ),
+                          TextFormField(
+                            controller: nameController,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Название не может быть пустым";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Название',
+                            ),
+//                                  onChanged: (str) {
+//                                    name = str;
+//                                  },
+                          ),
+                          TextField(
+                            controller: descController,
+                            decoration: InputDecoration(
+                              labelText: 'Описание',
+                            ),
+                            minLines: 2,
+                            maxLines: 5,
+                          ),
                         ],
-                        circleSize: 40,
                       ),
-                      TextFormField(
-                        controller: nameController,
-                        validator: (String value) {
-                          if (value.isEmpty) {
-                            return "Название не может быть пустым";
-                          }
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Название',
-                        ),
-                      ),
-                      TextField(
-                        controller: descController,
-                        decoration: InputDecoration(
-                          labelText: 'Описание',
-                        ),
-                        minLines: 2,
-                        maxLines: 5,
-                      ),
-                    ],
+                    ),
                   ),
-                  CardLayout(
-                    children: <Widget>[
-                      TextFormField(
-                        controller: durationController,
-                        validator: (String value) {
-                          if (value.isEmpty && value != null) {
-                            return "Укажите длительность";
-                          }
-//                                      else if (value ){
-//                                        return "Укажите длительность";
-//                                      }
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Длительность в минутах',
-                        ),
-                        keyboardType: TextInputType.number,
-                      ),
-                      TextFormField(
+                ),
+              ),
+              //last block w/ button
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    color: Theme.of(context).backgroundColor,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: <Widget>[
+                          //space inside block
+                          TextFormField(
                         controller: costController,
                         validator: (String value) {
                           if (value.isEmpty && value != null) {
@@ -165,43 +291,67 @@ class UserLessonEditState extends State<UserLessonEdit> {
 //                                        return "Укажите длительность";
 //                                      }
                         },
-                        decoration: InputDecoration(
-                          labelText: 'Цена',
-                        ),
-                        keyboardType: TextInputType.number,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: RaisedButton(
-                          onPressed: () {
-                            setState(() {
-                              if (_formKey.currentState.validate()) {
-                                editLesson(context);
-                              }
-                            });
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                'Обновить',
-                                style: TextStyle(
-                                    color: Theme.of(context).backgroundColor,
-                                    fontSize: 17),
-                              ),
-                            ],
+                            decoration: InputDecoration(
+                              labelText: 'Длительность в минутах',
+                            ),
+                            keyboardType: TextInputType.number,
+//                                  onChanged: (str) {
+//                                    duration = int.parse(str);
+//                                  },
                           ),
-                        ),
+                          TextFormField(
+                            controller: costController,
+                            validator: (String value) {
+                              if (value.isEmpty && value != null) {
+                                return "Укажите цену урока";
+                              }
+    //                                      else if (value ){
+    //                                        return "Укажите длительность";
+    //                                      }
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Цена',
+                            ),
+                            keyboardType: TextInputType.number,
+//                                    onChanged: (str) {
+//                                      cost = int.parse(str);
+//                                    },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: RaisedButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (_formKey.currentState.validate()) {
+                                    editLesson(context);
+                                  }
+                                });
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Создать',
+                                    style: TextStyle(
+                                        color:
+                                        Theme.of(context).backgroundColor,
+                                        fontSize: 17),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  )
-                ],
-              ),
-              ),
-            );
-          }),
-        )
-      ]));
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
 
   editLesson(BuildContext context) async {
     LessonModel lesson = new LessonModel(
@@ -222,3 +372,5 @@ class UserLessonEditState extends State<UserLessonEdit> {
     )..show(context);
   }
 }
+
+
